a6de617b3ea1f0cceeada8b9dbbe292a31a76c47
loud <loud@loud-20.04>
2023-04-13 18:15:41 -0400

    fixed variable names

--- client/main.py
+++ client/main.py
@@ -2,7 +2,7 @@ from main_window import AppRunner, MainWindow
 
 class FantasyWindow(MainWindow):
     def __init__(self, *args, **kwargs):
-        super().__init__('Team Tau Fantasy Football')
+        super().__init__(*args, **kwargs)
 
 if __name__ == '__main__':
     app = AppRunner(FantasyWindow)
