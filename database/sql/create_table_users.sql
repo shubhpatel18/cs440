741578903007bb61db79d0c9eaf639f265140d74
Shubh Patel <spp0010@mix.wvu.edu>
2023-04-22 12:22:50 -0400

    require unique usernames in db

--- database/sql/create_table_users.sql
+++ database/sql/create_table_users.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS public.users
 (
 	user_id serial,
-	username character varying(30) COLLATE pg_catalog."default" NOT NULL,
+	username character varying(30) UNIQUE COLLATE pg_catalog."default" NOT NULL,
 	name character varying(30) COLLATE pg_catalog."default",
 	password character varying(40) COLLATE pg_catalog."default" NOT NULL,
 	CONSTRAINT users_pkey PRIMARY KEY (user_id)
